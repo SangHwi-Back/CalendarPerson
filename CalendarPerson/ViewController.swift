bec99c5aac57ff8fd357bca7300e63b8148973e6
백상휘 <sanghwiback@gmail.com>
2021-10-25 00:04:29 +0900

    1차 storyBoard 작업 커밋

--- CalendarPerson/ViewController.swift
+++ CalendarPerson/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func testButtonTouchUpInside(_ sender: UIButton) {
+        performSegue(withIdentifier: "contentsSegue", sender: self)
+    }
+    
 }
 
